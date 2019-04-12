cde303ff8b83cd8ee98b04674bdefa0d0d1dab6f
utshomomen <mohibulmomenutsho@yahoo.com>
2019-04-12 12:11:54 +0300

    Update tests

--- modules/complex-number/test/test_utsho_momen_complex_number.cpp
+++ modules/complex-number/test/test_utsho_momen_complex_number.cpp
@@ -4,9 +4,8 @@
 
 #include "include/complex_number.h" 
 
-TEST(utsho_momen_ComplexNumberTest, Can_Create_Zero) {
-	// Arrange 
-	// Arrange
+TEST(Utsho_Momen_ComplexNumberTest, Can_Create_Zero) {
+    // Arrange
 	double re = 0.0;
 	double im = 0.0;
 
@@ -14,13 +13,12 @@ TEST(utsho_momen_ComplexNumberTest, Can_Create_Zero) {
 	ComplexNumber с3(re, im);
 
 	// Assert
-	EXPECT_EQ(re, с3.getRe());
-	EXPECT_EQ(im, с3.getIm());
+	EXPECT_EQ(re,с3.getRe());
+	EXPECT_EQ(im,с3.getIm());
 }
 
-TEST(utsho_momen_ComplexNumberTest, Multiplication_With_Two_Numbers) {
+TEST(Utsho_Momen_ComplexNumberTest, Multiplication_With_Two_Numbers) {
 	// Arrange 
-
 	ComplexNumber c1(5.0, 3.0);
 	ComplexNumber c2(4.0, 6.0);
 
@@ -28,19 +26,15 @@ TEST(utsho_momen_ComplexNumberTest, Multiplication_With_Two_Numbers) {
 	ComplexNumber c3 = c1 * c2;
 
 	// Assert
-
-
-	EXPECT_EQ(2, c3.getRe());
-	EXPECT_EQ(42, c3.getIm());
+	EXPECT_EQ(2,c3.getRe());
+	EXPECT_EQ(42,c3.getIm());
 
 }
 
-TEST(utsho_momen_ComplexNumberTest, Adding_Two_Numbers) {
+TEST(Utsho_Momen_ComplexNumberTest, Adding_Two_Numbers) {
 	// Arrange 
-
 	ComplexNumber c1(1.0, 3.0);
 	ComplexNumber c2(4.0, 6.0);
-
 
 	// Act
 	ComplexNumber c3 = c1 + c2;
@@ -50,13 +44,10 @@ TEST(utsho_momen_ComplexNumberTest, Adding_Two_Numbers) {
 	EXPECT_EQ(9, c3.getIm());
 }
 
-TEST(utsho_momen_ComplexNumberTest, Substracting_Number) {
-
-	// Arrange 
-
+TEST(Utsho_Momen_ComplexNumberTest, Substracting_Number) {
+    // Arrange 
 	ComplexNumber c1(1.0, 3.0);
 	ComplexNumber c2(4.0, 6.0);
-
 
 	// Act
 	ComplexNumber c3 = c1 - c2;
@@ -66,18 +57,15 @@ TEST(utsho_momen_ComplexNumberTest, Substracting_Number) {
 	EXPECT_EQ(-3, c3.getIm());
 }
 
-TEST(utsho_momen_ComplexNumberTest, Dividing_Two_Number) {
-
-	// Arrange 
-
+TEST(Utsho_Momen_ComplexNumberTest, Dividing_Two_Number) {
+    // Arrange 
 	ComplexNumber c1(4.0, 6.0);
 	ComplexNumber c2(2.0, 3.0);
 
-
-	// Act
+    // Act
 	ComplexNumber c3 = c1 / c2;
 
 	// Assert 
-	EXPECT_EQ(2, c3.getRe());
-	EXPECT_EQ(0, c3.getIm());
+	EXPECT_EQ(2,c3.getRe());
+	EXPECT_EQ(0,c3.getIm());
 }
