08e4f8e2a4c3a61e31283a850f8d22bd9d612d91
utshomomen <mohibulmomenutsho@yahoo.com>
2019-05-29 14:44:25 +0300

    lab2

---
+++ modules/prime-number/test/test_main.cpp
@@ -0,0 +1,10 @@
+// Copyright 2019 by Utsho Momen
+
+#include <iostream>
+#include "gtest/gtest.h"
+#include "include/PrimeNumber.h"
+
+int main(int argc, char **argv) {
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+}
