9c9c3243a298e166ded88517dad2c028d1fd9dba
utshomomen <32203690+utshomomen@users.noreply.github.com>
2019-05-30 01:29:45 +0300

    v.7

--- modules/prime-number/src/main.cpp
+++ modules/prime-number/src/main.cpp
@@ -3,20 +3,16 @@
 #include <iostream>
 
 #include "include/PrimeNumber.h"
-
 using namespace std;
 
 int main() {
-
-    int n ;
+    int n;
     cout << "Please enter number for which prime number to find : ";
     cin >> n;
-
-    if(n <= 0) {
+    if (n <= 0) {
         cout << "Invalid input" << endl;
-    }else {
-        cout << n << "th prime number is " << PrimeNumber(n) << endl;
+    } else {
+        cout << n << "the prime number is " << PrimeNumber(n) << endl;
     }
-
     return 0;
 }
